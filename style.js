091815f686d04bac58b5ed91476d669f8258f4f6
DavidBlink <39801596+DavidBlink@users.noreply.github.com>
2018-10-26 09:40:26 +0200

    Update style.js

--- style.js
+++ style.js
@@ -41,6 +41,7 @@
 				{ name: 'relaciona', element: 'h3', attributes: { 'class': 'bck-title26'} },
 				{ name: 'fondo verde', element: 'h3', attributes: { 'class': 'bck-title31'} },
 				{ name: 'argumentar azul', element: 'h3', attributes: { 'class': 'bck-title27'} },
+				{ name: 'argumentar verde', element: 'h3', attributes: { 'class': 'bck-title35'} },
 				{ name: 'form ciervoybuitre', element: 'h3', attributes: { 'class': 'bck-title28'} },
 				{ name: 'Pasado al presente', element: 'h3', attributes: { 'class': 'bck-title29'} },
 				{ name: 'epa', element: 'h3', attributes: { 'class': 'bck-title30'} },
