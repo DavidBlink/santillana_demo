dd2ce677e6ea55452df22e5047735177ef856ab6
DavidBlink <39801596+DavidBlink@users.noreply.github.com>
2018-11-15 12:54:50 +0100

    Update style.js

--- style.js
+++ style.js
@@ -99,6 +99,7 @@
 				{ name: 'Lista Des blanca', element: 'ul', attributes: { 'class': 'bck-ul-5'} },
 				{ name: 'Lista Des morado', element: 'ul', attributes: { 'class': 'bck-ul-6'} },
 				{ name: 'Lista Des rojo', element: 'ul', attributes: { 'class': 'bck-ul-7'} },
+				{ name: 'Lista Cuadrada rojo', element: 'ul', attributes: { 'class': 'bck-ul-8'} },
 				
 				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-1' } },
 				{ name: 'Caja marron', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-2' } },
